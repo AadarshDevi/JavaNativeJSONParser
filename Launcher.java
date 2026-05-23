6ea537e833473643541ae07fe5bab7aec7389755
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-23 12:57:57 -0700

    replaced testJSON with arrays instead of values

--- Launcher.java
+++ Launcher.java
@@ -1,36 +1,28 @@
 public class Launcher {
 	static void main() {
-//		JSON json = new JSON();
-//		json.append("bool", "Hola Mr. Car");
-//		IO.println(json);
-
 		String testJSON = """
 			{
-			  "string": "trident",
-			  "bool": null,
-			  "int": 12,
-			  "double": 134.25,
 			  "string_arr": [
-			    "item 1",
-			    "item 2",
-			    "item 3"
-			  ],
-			  "int_arr": [
-			    18,
-			    23,
-			    34
-			  ],
-			  "double_arr": [
-			    12.45,
-			    7256.78,
-			    25867.70
-			  ],
-			  "bool_arr": [
-			    true,
-			    true,
-			    false,
-			    true
-			  ]
+			        "item 1",
+			        "item 2",
+			        "item 3"
+			      ],
+			      "int_arr": [
+			        18,
+			        23,
+			        34
+			      ],
+			      "double_arr": [
+			        12.45,
+			        7256.78,
+			        25867.70
+			      ],
+			      "bool_arr": [
+			        true,
+			        true,
+			        false,
+			        true
+			      ]
 			}
 			""";
 
