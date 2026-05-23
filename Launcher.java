801bee8cbadfeeaa7693fe5df0b216104a6e9b2a
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-23 12:17:56 -0700

    random issue

--- Launcher.java
+++ Launcher.java
@@ -1,8 +1,5 @@
 public class Launcher {
 	static void main() {
-		JSON json = new JSON();
-		json.append("bool", (String) null);
-		IO.println(json);
 //		JSON json = new JSON();
 //		json.append("bool", "Hola Mr. Car");
 //		IO.println(json);
