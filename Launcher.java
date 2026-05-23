7d2637c91a0089eac4ce069dc530c8dc7e1889a7
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-05-23 13:10:21 -0700

    easier time to parse data

--- Launcher.java
+++ Launcher.java
@@ -2,27 +2,12 @@ public class Launcher {
 	static void main() {
 		String testJSON = """
 			{
-			  "string_arr": [
-			        "item 1",
-			        "item 2",
-			        "item 3"
-			      ],
-			      "int_arr": [
-			        18,
-			        23,
-			        34
-			      ],
-			      "double_arr": [
-			        12.45,
-			        7256.78,
-			        25867.70
-			      ],
-			      "bool_arr": [
-			        true,
-			        true,
-			        false,
-			        true
-			      ]
+			  "double": 134.25,
+			   "string_arr": [
+				 "item 1",
+				 "item 2",
+				 "item 3"
+			   ]
 			}
 			""";
 
